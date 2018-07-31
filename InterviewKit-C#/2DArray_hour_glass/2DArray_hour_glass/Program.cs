83e6ee1495dd4b2c625e83fe541fcce85c553a69
Albert Kliemke <albertkliemke@gmail.com>
2018-07-30 23:37:17 -0300

    C# Hourglass 2D Array.

---
+++ InterviewKit-C#/2DArray_hour_glass/2DArray_hour_glass/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DArray_hour_glass
+{
+    class Program
+    {
+        // Complete the hourglassSum function below.
+        static int hourglassSum(int[][] arr)
+        {
+
+            for (int interations = 16; interations > 0; interations--)
+            {
+
+            }
+
+            return 0;
+
+        }
+
+        static void Main(string[] args)
+        {
+            
+
+            int[][] arr = new int[6][];
+
+            for (int i = 0; i < 6; i++)
+            {
+                arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            }
+
+            int result = hourglassSum(arr);
+
+           // textWriter.WriteLine(result);
+        }
+    }
+}
